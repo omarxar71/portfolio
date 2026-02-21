371ce7e13bebad5f5dc826c88a42f674a4c62692
omarxar71 <omarabdrabi123@gmail.com>
2026-02-21 05:42:28 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/dist/', // Add this line!
+  base: '/portfolio/', // Add this line!
 })
