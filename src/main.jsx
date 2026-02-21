dd4d0cc8a89fe9522f71d9f006e89c55c4a916c7
omarxar71 <omarabdrabi123@gmail.com>
2026-02-21 05:57:54 +0200

    Fix: Hardcoding base path and homepage

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
